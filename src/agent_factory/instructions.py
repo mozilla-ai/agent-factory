ef063e4e57fd76347220cead4d8972b91a515b06
Hareesh Bahuleyan <hareeshbahuleyan@gmail.com>
2025-06-17 14:49:14 +0200

    Use uv instead of mamba, save venv inside generated_workflows/

--- src/agent_factory/instructions.py
+++ src/agent_factory/instructions.py
@@ -178,7 +178,7 @@ The final output should be a JSON with the following structure:
 1. agent_code should contain all the code implementation of the agent which will correspond to the runnable agent.py script
 2. run_instructions should contain clear and concise setup instructions:
     - Environment variables: Instruct the user to create a .env file to set environment variables; specify exactly which environment variables are required
-    - Setting up the environment via mamba (Python version 3.11)
+    - Setting up the environment via uv (Python version 3.11). The python environment should be created as 'generated_workflows/latest/workflow_venv'
     - Installing dependencies via requirements.txt
     - Run instructions for agent.py
 3. dependencies should list all the python libraries (including the ones required by the tools) as dependencies to be installed. It will be used to generate the requirements.txt file
