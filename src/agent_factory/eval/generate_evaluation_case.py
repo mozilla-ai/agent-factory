594562d571473c89ab2598af2aace9d32bc8c1f1
Javier Torres <javier@mozilla.ai>
2025-09-01 17:41:52 +0200

    Fix stdio process cleanup

--- src/agent_factory/eval/generate_evaluation_case.py
+++ src/agent_factory/eval/generate_evaluation_case.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import dotenv
+import asyncio
 import fire
 from any_agent import AgentConfig, AgentFramework, AnyAgent
 from any_agent.config import MCPStdio
@@ -58,27 +59,6 @@ def main(
         ".",
     ]
 
-    agent = AnyAgent.create(
-        agent_framework=framework,
-        agent_config=AgentConfig(
-            model_id=model,
-            instructions=get_instructions(generated_workflow_dir),
-            tools=[
-                visit_webpage,
-                search_tavily,
-                MCPStdio(
-                    command="npx",
-                    args=tool_args,
-                    tools=[
-                        "read_file",
-                        "list_directory",
-                    ],
-                ),
-            ],
-            output_type=JSONEvaluationCase,
-        ),
-    )
-
     run_instructions = """
     Read the {generated_workflow_dir}/agent.py script and generate a JSON evaluation case for it.
     The criteria generated must:
@@ -88,7 +68,36 @@ def main(
     - be solely based on the agent's INSTRUCTIONS, tools in the agent configuration (including MCPs) and output_type JSON structured output.
     You may ignore the framework name and model_id in the agent configuration.
     """  # noqa: E501
-    agent_trace = agent.run(run_instructions.format(generated_workflow_dir=generated_workflow_dir), max_turns=30)
+
+    async def eval_async_fun():
+        agent = await AnyAgent.create_async(
+            agent_framework=framework,
+            agent_config=AgentConfig(
+                model_id=model,
+                instructions=get_instructions(generated_workflow_dir),
+                tools=[
+                    visit_webpage,
+                    search_tavily,
+                    MCPStdio(
+                        command="npx",
+                        args=tool_args,
+                        tools=[
+                            "read_file",
+                            "list_directory",
+                        ],
+                    ),
+                ],
+                output_type=JSONEvaluationCase,
+            ),
+        )
+        ag_trace = await agent.run_async(run_instructions.format(generated_workflow_dir=generated_workflow_dir), max_turns=30)
+        for server in agent._mcp_servers:
+            await server.mcp_connection.server.cleanup()
+        return ag_trace
+
+    agent_trace = asyncio.run(
+        eval_async_fun()
+    )
 
     cost_info = agent_trace.cost
     evaluation_case_generation_costs = {
